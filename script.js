a437bbae74faba985c87b4377fc6557d7ed9fdff
vineeth <119692597+vineeth38@users.noreply.github.com>
2023-04-10 18:23:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+function firstNonRepeatingCharacter(str) {
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i];
+    if (str.indexOf(char) == i && str.indexOf(char, i + 1) == -1) {
+      return char;
+    }
+  }
